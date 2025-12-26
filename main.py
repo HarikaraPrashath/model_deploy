752b8a10f2c04a7dec16215632559d529fda50d7
Harikara Prashath <121121318+HarikaraPrashath@users.noreply.github.com>
2025-12-26 18:09:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,17 +25,9 @@ label_enc = joblib.load(ENCODER_PATH)
 # -------------------------------------------------
 app = FastAPI()
 
-origins = [
-    o.strip() for o in os.getenv(
-        "ALLOWED_ORIGINS",
-        "http://localhost:3000,http://127.0.0.1:3000"
-    ).split(",")
-    if o.strip()
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
