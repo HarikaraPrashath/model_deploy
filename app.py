3ec3fb5f5a7f07521573d95d23521d5de5f4008e
HarikaraPrashath <prashathhari2002@gmail.com>
2026-03-04 14:24:08 +0530

    CHnage the model

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from typing import Optional
-from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from model_schema.schema import StudentInput
+from model_schema.student_model import StudentInputGuide
 from service.Career_preddiction import predict_career_service
 
 #files are import from service folder
 from service.career_guide_service import predict_career
 
 #this is Prevent the CORS issues
-app = FastAPI(title="Career Prediction API")
+app = FastAPI(title="Career Prediction")
 
 app.add_middleware(
     CORSMiddleware,
@@ -20,37 +20,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-class StudentInput(BaseModel):
-    Soft_Skills: str
-    Key_Skils: str
-    Current_semester: str
-    Learning_Style: str
-    GPA: float
-    English_score: float
-    Ocean_Openness: float
-    Ocean_Conscientiousness: float
-    Ocean_Extraversion: float
-    Ocean_Agreeableness: float
-    Ocean_Neuroticism: float
-    Riasec_Realistic: float
-    Riasec_Investigative: float
-    Riasec_Artistic: float
-    Riasec_Social: float
-    Riasec_Enterprising: float
-    Riasec_Conventional: float
-
-    # Make optional
-    Is_Sliit_Student: Optional[bool] = False
-    Specialization: Optional[str] = ""
-
 #this is the root path
 @app.get("/")
 def root():
     return {"status": "Career Prediction API running"}
 
 @app.post("/predict-career")
-def predict(inp: StudentInput):
-    return predict_career(inp)
+def predict_career(student: StudentInputGuide):
+    return predict_career(student)
 
 @app.post("/predict")
 def predict_career(student: StudentInput):
